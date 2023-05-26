f03e37fe1514990f336ef4a721ebe67ec5f93006
MasaakiPetiteau <133872934+MasaakiPetiteau@users.noreply.github.com>
2023-05-26 09:17:06 +0200

    Update Code_EcoSquare.ino

--- Code_EcoSquare.ino
+++ Code_EcoSquare.ino
@@ -1,8 +1,8 @@
 // TEMPERATURE :
 #include <math.h>
-const int B = 4275;               // B value of the thermistor
-const int R0 = 100000;            // R0 = 100k
-const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
+const int B = 4275;
+const int R0 = 100000;
+const int pinTempSensor = A0; // le capteur de température est connecté à la boche A0
 #if defined(ARDUINO_ARCH_AVR)
 #define debug  Serial
 #elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
@@ -12,19 +12,20 @@ const int pinTempSensor = A0;     // Grove - Temperature Sensor connect to A0
 #endif
 
 // HUMIDITE :
-int sensorPin = A1;
+int sensorPin = A1; le capteur d'humidité est connecté à la boche A1
 int humidite = 0;
 
 // POMPE :
-const int bouton = 3; // le bouton est connecté à la broche 3 de la carte Adruino
-const int relais_pompe = 4; // // le relais est connecté à la broche 4 de la carte Adruino
+const int bouton = 3; // le bouton est connecté à la broche 3
+const int relais_pompe = 4; // le relais est connecté à la broche 4
 int etatBouton;
+
 
 void setup()
 {
-    Serial.begin(20000);
+    Serial.begin(9600);
     pinMode(bouton, INPUT); // le bouton est une entrée
-    pinMode(relais_pompe, OUTPUT);
+    pinMode(relais_pompe, OUTPUT); // le relais est une sortie
 
     
 }
@@ -36,13 +37,13 @@ void loop()
     float R = 1023.0/a-1.0;
     R = R0*R;
     float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15+12.85; // convert to temperature via datasheet // ++ correction (+12.85°C)
-    Serial.print("temperature = ");
+    Serial.print("Température = ");
     Serial.println(temperature);
     delay(100);
 
     // HUMIDITE :
     humidite = analogRead(sensorPin);
-    Serial.print("Moisture = " );
+    Serial.print("Humidité = " );
     Serial.println(humidite);
     delay(1000);
 
